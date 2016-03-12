a5e121bffaf4496d0dc893a339971477e10279b1
Adarsh Jayakumar <adarshjaya12@gmail.com>
2016-03-11 22:38:42 -0600

    First Commit Bills Burger

---
+++ HandBurger/src/adarshjayakumar/com/Main.java
@@ -0,0 +1,9 @@
+package adarshjayakumar.com;
+// Bills Burger Company
+//Process of selling hamburgs
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
