12c9dedc4c20f40fb9233289811dd5c062aa97dd
Adarsh Jayakumar <adarshjaya12@gmail.com>
2016-03-15 01:01:49 -0500

    sort result , fixture

--- GenericApplication/src/adarshjayakumar/com/Main.java
+++ GenericApplication/src/adarshjayakumar/com/Main.java
@@ -20,6 +20,7 @@ public class Main {
         manchesterUnited.addPlayer(unitedPlayer3);
         manchesterUnited.addPlayer(unitedPlayer4);
         manchesterUnited.addPlayer(unitedPlayer5);
+
         //SoccerPlayer  = new SoccerPlayer("");
 
         SoccerPlayer chelseaPlayer1 = new SoccerPlayer("Costa");
@@ -36,7 +37,7 @@ public class Main {
 
 
         SoccerPlayer  liverpoolPlayer1 = new SoccerPlayer("Furminio");
-        SoccerPlayer liverpoolPlayer2= new SoccerPlayer("Henderson");
+        SoccerPlayer liverpoolPlayer2 = new SoccerPlayer("Henderson");
         SoccerPlayer  liverpoolPlayer3= new SoccerPlayer("Miller");
         SoccerPlayer  liverpoolPlayer4= new SoccerPlayer("Sturridge");
         SoccerPlayer  liverpoolPlayer5= new SoccerPlayer("Cuntinio");
@@ -59,19 +60,120 @@ public class Main {
         manchesterCity.addPlayer(mancityPlayer3);
         manchesterCity.addPlayer(mancityPlayer4);
         manchesterCity.addPlayer(mancityPlayer5);
-        
+        System.out.println("Fixtures Result : \n");
+        manchesterUnited.matchResult(manchesterCity,3,1);
+        manchesterUnited.matchResult(chelsea,4,3);
+        manchesterUnited.matchResult(liverpool,2,1);
+        manchesterUnited.matchResult(manchesterCity,5,3);
+        manchesterUnited.matchResult(chelsea,7,1);
+        manchesterUnited.matchResult(liverpool,2,2);
+
+        chelsea.matchResult(manchesterCity,2,1);
+        chelsea.matchResult(liverpool,1,1);
+        chelsea.matchResult(manchesterCity,2,4);
+        chelsea.matchResult(liverpool,1,2);
+
+        manchesterCity.matchResult(liverpool,1,2);
+        manchesterCity.matchResult(liverpool,2,2);
+
+        /*chelsea.matchResult(manchesterCity,3,1);
+        chelsea.matchResult(manchesterUnited,4,3);
+        chelsea.matchResult(liverpool,2,1);
+        chelsea.matchResult(manchesterCity,5,3);
+        chelsea.matchResult(manchesterUnited,7,1);
+        chelsea.matchResult(liverpool,2,2);
+
+        manchesterUnited.matchResult(manchesterCity,3,1);
+        manchesterUnited.matchResult(chelsea,4,3);
+        manchesterUnited.matchResult(liverpool,2,1);
+        manchesterUnited.matchResult(manchesterCity,5,3);
+        manchesterUnited.matchResult(chelsea,7,1);
+        manchesterUnited.matchResult(liverpool,2,2);
+
+        manchesterUnited.matchResult(manchesterCity,3,1);
+        manchesterUnited.matchResult(chelsea,4,3);
+        manchesterUnited.matchResult(liverpool,2,1);
+        manchesterUnited.matchResult(manchesterCity,5,3);
+        manchesterUnited.matchResult(chelsea,7,1);
+        manchesterUnited.matchResult(liverpool,2,2);*/
+
+
+
         epl.addTeam(manchesterUnited);
         epl.addTeam(chelsea);
         epl.addTeam(liverpool);
         epl.addTeam(manchesterCity);
         epl.showLeagueTable();
 
+        LeagueTable <Team<CricketPlayer>> ipl = new LeagueTable<>("English Premium League");
+        Team<CricketPlayer> csk = new Team<>("Chennai Super kings");
+        Team<CricketPlayer> kxip = new Team<>("Kings XI punjab");
+        Team<CricketPlayer> rcb = new Team<>("Royal challengers Banglore");
+        Team<CricketPlayer> mi = new Team<>("Mumbai Indians");
+
+        CricketPlayer cskPlayer1 = new CricketPlayer("MS Dhoni");
+        CricketPlayer cskPlayer2 = new CricketPlayer("Suresh Raina");
+        CricketPlayer cskPlayer3 = new CricketPlayer("R Ashwin");
+        CricketPlayer cskPlayer4 = new CricketPlayer("Brendon Mcculumm");
+        CricketPlayer cskPlayer5 = new CricketPlayer("Dwayne Smith");
+        csk.addPlayer(cskPlayer1);
+        csk.addPlayer(cskPlayer2);
+        csk.addPlayer(cskPlayer3);
+        csk.addPlayer(cskPlayer4);
+        csk.addPlayer(cskPlayer5);
+
+        CricketPlayer kxipPlayer1 = new CricketPlayer("Yuvraj");
+        CricketPlayer kxipPlayer2 = new CricketPlayer("Maxwel");
+        CricketPlayer kxipPlayer3 = new CricketPlayer("Vijay");
+        CricketPlayer kxipPlayer4 = new CricketPlayer("George Bailey");
+        CricketPlayer kxipPlayer5 = new CricketPlayer("Miller");
+        kxip.addPlayer(kxipPlayer1);
+        kxip.addPlayer(kxipPlayer2);
+        kxip.addPlayer(kxipPlayer3);
+        kxip.addPlayer(kxipPlayer4);
+        kxip.addPlayer(kxipPlayer5);
+
+        CricketPlayer rcbPlayer1 = new CricketPlayer("Kholi");
+        CricketPlayer rcbPlayer2 = new CricketPlayer("ABD");
+        CricketPlayer rcbPlayer3 = new CricketPlayer("Gayle");
+        CricketPlayer rcbPlayer4 = new CricketPlayer("Starc");
+        CricketPlayer rcbPlayer5 = new CricketPlayer("Watson");
+        rcb.addPlayer(rcbPlayer1);
+        rcb.addPlayer(rcbPlayer2);
+        rcb.addPlayer(rcbPlayer3);
+        rcb.addPlayer(rcbPlayer4);
+        rcb.addPlayer(rcbPlayer5);
+
+        CricketPlayer miPlayer1 = new CricketPlayer("Sharma");
+        CricketPlayer miPlayer2 = new CricketPlayer("Malinga");
+        CricketPlayer miPlayer3 = new CricketPlayer("Rayudu");
+        CricketPlayer miPlayer4 = new CricketPlayer("Harbajan");
+        CricketPlayer miPlayer5 = new CricketPlayer("Sachin");
+        mi.addPlayer(miPlayer1);
+        mi.addPlayer(miPlayer2);
+        mi.addPlayer(miPlayer3);
+        mi.addPlayer(miPlayer4);
+        mi.addPlayer(miPlayer5);
+        System.out.println("Fixture Result : \n");
+        csk.matchResult(kxip,1,0);
+        csk.matchResult(kxip,1,0);
+        csk.matchResult(rcb,1,0);
+        csk.matchResult(rcb,0,1);
+        csk.matchResult(mi,1,0);
+        csk.matchResult(mi,0,1);
+        kxip.matchResult(rcb,1,0);
+        kxip.matchResult(rcb,0,1);
+        kxip.matchResult(mi,1,0);
+        kxip.matchResult(mi,0,1);
+        rcb.matchResult(mi,1,0);
+        rcb.matchResult(mi,0,1);
 
 
-
-
-
-
+        ipl.addTeam(csk);
+        ipl.addTeam(kxip);
+        ipl.addTeam(rcb);
+        ipl.addTeam(mi);
+        ipl.showLeagueTable();
 
 
 
