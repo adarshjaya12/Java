b111e34f93e837c1935eab840d533242227a501c
Adarsh Jayakumar <adarshjaya12@gmail.com>
2016-03-15 17:57:57 -0500

    Testing Series Library

---
+++ package/src/com/adarsh/Main.java
@@ -0,0 +1,12 @@
+package com.adarsh;
+
+import com.series.Series;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println(" The factorial of 20 is : "+Series.factorial(20));
+        System.out.println("The fibonacci of 90 is : "+Series.fibonacci(90));
+        System.out.println("The sum of 39 is : "+Series.nSum(39));
+  }
+}
