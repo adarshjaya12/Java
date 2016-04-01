fddd1d9512b72b1d54b108fbe11ab62b23d88f53
Adarsh Jayakumar <adarshjaya12@gmail.com>
2016-03-31 19:02:16 -0500

    Dummy

---
+++ FizzBuzz/src/com/adarsh/Main.java
@@ -0,0 +1,15 @@
+package com.adarsh;
+
+class staticClass{
+    public int variable1;
+    public void display()
+    {
+        System.out.println("The value is "+variable1+"and the address is"+&variable1);
+    }
+}
+public class Main {
+
+    public static void main(String[] args) {
+
+    }
+}
